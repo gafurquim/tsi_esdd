8df40eba862c437523ab7738357400e200d53743
Gustavo <gafurquim@gmail.com>
2025-08-18 12:43:50 -0300

    Commiting

--- aula01-Revisao/ponteiros.c
+++ aula01-Revisao/ponteiros.c
@@ -8,6 +8,11 @@
 int main() {
     int a = 10;
     int *p = &a;
-    printf("%d\n", *p);
+
+    printf("Valor da variavel a: %d\n", a);
+    printf("Endereco da variavel a: %p\n", &a);
+    printf("Valor do ponteiro p: %d\n", p);
+    printf("Valor do objeto apontado por p (valor de a): %d\n", *p);
+    
     return 0;
 }
