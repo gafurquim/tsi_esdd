bf4735bc0d6037aa0c640e7746e9432f290ffdc8
Gustavo <gafurquim@gmail.com>
2025-09-08 19:37:57 -0300

    Committed on Mon Sep  8 07:37:57 PM -03 2025

---
+++ aula03-DMA/05_dma.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  int array[5] = {10, 20, 30, 40, 50};
+
+  printf("Tamanho de um inteiro: %ld bytes\n", sizeof(int));
+
+  printf("Indice | array[i] | *(array + i) | i[array] | Endereço\n");
+  printf("-------|----------|--------------|----------|---------------\n");
+
+  for (int i = 0; i < 5; i++) {
+      printf(" %d     |   %d     |    %d        |   %d     | %p\n",
+          i,
+          array[i],
+          *(array + i),
+          i[array],
+          (void*)(array + i)
+      );
+  }
+
+  return 0;
+}
